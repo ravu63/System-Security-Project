71e198e7120c2aae48fbe71c8982192b4e485ac9
chestontjf <chestontanjf@gmail.com>
2022-07-01 22:43:26 +0800

    database halfway 1

--- __init__.py
+++ __init__.py
@@ -74,7 +74,8 @@ class Transaction(db.Model):
     id = db.Column(db.Interger, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(30), nullable=False)
-
+    address = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(min=30,max=100), nullable=False)
 
 
 # end Create table
