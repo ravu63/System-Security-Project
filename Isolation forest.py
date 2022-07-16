ded4d3f5356233828f227a8b6c7911f7f2d04f34
LiewHongXun <96364795+LiewHongXun@users.noreply.github.com>
2022-07-16 21:46:41 +0800

    Minor change to Isolation forest.py

--- Isolation forest.py
+++ Isolation forest.py
@@ -2,6 +2,67 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.ensemble import IsolationForest
+import random
+import datetime
+import string
+from flask import Flask, render_template, request, redirect, url_for, session, flash, Response, make_response
+from flask_mail import Mail, Message
+from Feedback1 import Feedback1
+from Forms import OTPform, \
+    CreateLoanForm, CreatePlanForm, PawnCreation, \
+    PawnStatus, \
+    PawnRetrieval, SearchSUI, filterStatus, FeedbackForm1
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, PasswordField, SelectField, TextAreaField, \
+    SubmitField
+from wtforms.fields import EmailField, DateField
+from wtforms.validators import ValidationError
+from transaction import CustomerPurchase
+from Currency import Currency
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
+from flask_bcrypt import Bcrypt
+from captcha_generate import generate_captcha_image
+from datetime import datetime, date
+import pytz
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+import cv2
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import os
+import datetime
+from getmac import get_mac_address as gma
+import socket
+
+KEY = "8341c4d3ee5842ea9ab5a2f9192a020a"
+ENDPOINT = "https://radiant63.cognitiveservices.azure.com/"
+face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+blob_service_client = BlobServiceClient.from_connection_string(
+    "DefaultEndpointsProtocol=https;AccountName=ravu63;AccountKey=Rb3XX8PGQswVKFQPNYGjh+d/1+s98EOyNvltfbuoL89v0c7HcTGa7bPykBuaD8A0FkEVWuNokhhn+AStJVn5+w==;EndpointSuffix=core.windows.net")
+
+s = URLSafeTimedSerializer('ThisIsASecret!')
+app = Flask(__name__)
+app.debug = True
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_DEBUG'] = True
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = "radiantfinancenyp@gmail.com"
+app.config['MAIL_PASSWORD'] = "xepjuxdlrsmpcnxk"
+mail = Mail(app)
+bcrypt = Bcrypt(app)
+
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+
+class LoginData(db.Model):
+    LoginEntry = db.Column(db.Integer, primary_key=True)
+    LoginEmail = db.Column(db.String(50), nullable=False)
+    LoginTime = db.Column(db.Time(), nullable=False)
+
 
 data = pd.read_csv('marks.csv')
 data.head(10)
