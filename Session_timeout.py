d32bb9974f5ab7a8b3f1771bd8080bc0bde1ea87
ravu63 <ravuthasamypremkumar@gmail.com>
2022-08-14 01:15:12 +0800

    Removal

--- Session_timeout.py
+++
@@ -1,7 +0,0 @@
-from datetime import timedelta
-from flask import session, app
-
-@app.before_request
-def make_session_permanent():
-    session.permanent = True
-    app.permanent_session_lifetime = timedelta(minutes=5)
